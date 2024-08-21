d58747be33e54774ce8d1b23bf38e1c518b5e493
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-08-21 15:03:49 +0000

    update on phrase again

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home(){
     return (
         <div style="background-color: #ffe4e1; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); padding: 20px; max-width: 300px; text-align: center;">
             <h1 style="color: #ff6f61; font-size: 24px; margin-bottom: 10px;">Te amo meu xuxu ❤️</h1>
-            git <p style="font-size: 40px; color: #ff6f61;">❤️</p>
+            <p style="font-size: 40px; color: #ff6f61;">❤️</p>
         </div>
     )
 }
