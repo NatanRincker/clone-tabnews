15f6777deb2474edf082e68781589ecf466940ed
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-08-22 13:14:12 +0000

    teste 2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 export default function Home(){
     return (
-        <div style="background-color: #ffe4e1; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); padding: 20px; max-width: 300px; text-align: center;">
-            <h1 style="color: #ff6f61; font-size: 24px; margin-bottom: 10px;">Te amo meu xuxu ❤️</h1>
-            <p style="font-size: 40px; color: #ff6f61;">❤️</p>
-        </div>
+        <h1>Teste 2</h1>
     )
 }
