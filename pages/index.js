9490a02fc0767588becdd62342b60fcf3830801e
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-08-25 21:25:29 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,27 @@
-export default function Home(){
-    const cardStyle = {
-        backgroundColor: '#ffe4e1',
-        borderRadius: '15px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-        padding: '20px',
-        maxWidth: '300px',
-        textAlign: 'center',
-    };
-    
-    const titleStyle = {
-        color: '#ff6f61',
-        fontSize: '24px',
-        marginBottom: '5px',
-    };
-    
-    const emojiStyle = {
-        fontSize: '60px',
-        color: '#ff6f61',
-    };
-    return (
-        <div style={cardStyle}>
-          <h1 style={titleStyle}>Te amo meu xuxuzinho</h1>
-          <p style={emojiStyle}>❤️</p>
-        </div>
-    );
+export default function Home() {
+  const cardStyle = {
+    backgroundColor: "#ffe4e1",
+    borderRadius: "15px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+    padding: "20px",
+    maxWidth: "300px",
+    textAlign: "center",
+  };
+
+  const titleStyle = {
+    color: "#ff6f61",
+    fontSize: "24px",
+    marginBottom: "5px",
+  };
+
+  const emojiStyle = {
+    fontSize: "60px",
+    color: "#ff6f61",
+  };
+  return (
+    <div style={cardStyle}>
+      <h1 style={titleStyle}>Te amo meu xuxuzinho</h1>
+      <p style={emojiStyle}>❤️</p>
+    </div>
+  );
 }
-
-
-
-  
-    
