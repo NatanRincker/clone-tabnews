af4390f1ccf66924dee88efef973b43e1b70a499
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-09-06 23:44:07 +0000

    adds files `.env` and `database.js` | `compose.yaml` addapted to use env. variables as well as in `database,js` | connetion between client and server stablished returning http 200 | installed npm module `pg`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,9 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+
+  console.log(result);
+
   response.status(200).json({ chave: "Bora ficar rico" });
 }
