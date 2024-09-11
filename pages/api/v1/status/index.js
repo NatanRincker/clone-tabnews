524d654452d2f3bf261cfe88609e6b35b1ec40f3
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-09-11 01:11:04 +0000

    Added PostgreSQL version and max connections to the `/api/v1/status` response. | Included `updated_at` timestamp in the response. | Updated integration tests to validate the new fields (`pg_version`, `pg_max_connections`, and `updated_at`).

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
+  const updatedAt = new Date().toISOString();
+  const queryVersion = await database.query("SELECT VERSION();");
+  const pgVersion = queryVersion.rows[0].version;
 
-  console.log(result);
+  const queryMaxConnections = await database.query("SHOW max_connections;");
+  const pgMaxConnectionsBuffer = queryMaxConnections.rows[0].max_connections;
+  let pgMaxConnections =
+    typeof pgMaxConnectionsBuffer == "string" ? pgMaxConnectionsBuffer : -1;
+  pgMaxConnections = Number(pgMaxConnections);
+  pgMaxConnections = isNaN(pgMaxConnections) ? -1 : pgMaxConnections;
 
-  response.status(200).json({ chave: "Bora ficar rico" });
+  response.status(200).json({
+    updated_at: updatedAt,
+    pg_version: pgVersion,
+    pg_max_connections: pgMaxConnections,
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
-test("GET to /aoi/v1/sutatus should return 200", async () => {
+test("API EndPoint | GET | ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  console.log(responseBody);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const pgVersion = responseBody.pg_version;
+  expect(typeof pgVersion).toBe("string");
+  pgVersionIsEmpty = pgVersion.length > 0 ? false : true;
+  expect(pgVersionIsEmpty).toBe(false);
+
+  const pgMaxConnections = responseBody.pg_max_connections;
+  expect(typeof pgMaxConnections).toBe("number");
 });
