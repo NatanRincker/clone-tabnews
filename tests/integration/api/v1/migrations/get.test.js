586254c0b21ec00499cfdadeac658b6a23f11007
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-10-02 14:42:09 +0000

    adds `api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,24 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query(
+    "drop schema if exists public cascade; create schema public;",
+  );
+}
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  console.log(`## NODE_ENV: ${process.env.NODE_ENV}`);
+
+  const queryResult = await database.query("SELECT 1 + 1 as sum;");
+  expect(queryResult.rows[0].sum).toBe(2);
+
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
