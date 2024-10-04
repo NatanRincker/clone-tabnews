bf4506c3d9ef9482e0f0e75ebd7504562532ba79
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-10-04 13:50:25 +0000

    added try/catch to the bug fix to make it more robust

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,31 +3,39 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  let migrationsResponse = {};
-  let statusCode;
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    statusCode = 200;
-    migrationsResponse = pendingMigrations;
-  } else if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    statusCode = migratedMigrations.length > 0 ? 201 : 200;
-    migrationsResponse = migratedMigrations;
-  } else {
-    statusCode = 405; // Method Not Allowed
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+    let migrationsResponse;
+    let statusCode;
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      statusCode = 200;
+      migrationsResponse = pendingMigrations;
+    } else if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      statusCode = migratedMigrations.length > 0 ? 201 : 200;
+      migrationsResponse = migratedMigrations;
+    } else {
+      statusCode = 405; // Method Not Allowed
+      migrationsResponse = { error: `Method ${request.method} Not Allowed` };
+    }
+    return response.status(statusCode).json(migrationsResponse);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
-  await dbClient.end();
-  return response.status(statusCode).json(migrationsResponse);
 }
