3537e5343327b14f7da15b7dd9cbac3dfd42f82e
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2024-10-31 14:19:38 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,18 +6,21 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  console.log(`## NODE_ENV: ${process.env.NODE_ENV}`);
+describe("GET to /api/v1/migrations", () => {
+  describe("Anonymous User Running pending migrations", () => {
+    test("should return 200", async () => {
+      console.log(`## NODE_ENV: ${process.env.NODE_ENV}`);
 
-  const queryResult = await database.query("SELECT 1 + 1 as sum;");
-  expect(queryResult.rows[0].sum).toBe(2);
+      const queryResult = await database.query("SELECT 1 + 1 as sum;");
+      expect(queryResult.rows[0].sum).toBe(2);
 
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  console.log(responseBody);
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,28 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody).toBeDefined();
-  console.log(responseBody);
+      const responseBody = await response.json();
+      expect(responseBody).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  const databaseStatus = responseBody.dependencies.database;
-  expect(databaseStatus).toBeDefined();
+      const databaseStatus = responseBody.dependencies.database;
+      expect(databaseStatus).toBeDefined();
 
-  expect(databaseStatus.version).toBe("16.4");
+      expect(databaseStatus.version).toBe("16.4");
 
-  expect(typeof databaseStatus.max_connections).toBe("number");
-  expect(databaseStatus.max_connections).toBeGreaterThan(0);
+      expect(typeof databaseStatus.max_connections).toBe("number");
+      expect(databaseStatus.max_connections).toBeGreaterThan(0);
 
-  expect(typeof databaseStatus.current_connections).toBe("number");
-  expect(databaseStatus.current_connections).toBeGreaterThan(-1);
+      expect(typeof databaseStatus.current_connections).toBe("number");
+      expect(databaseStatus.current_connections).toBeGreaterThan(-1);
+    });
+  });
 });
